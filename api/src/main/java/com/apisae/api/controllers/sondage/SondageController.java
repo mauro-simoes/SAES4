688d2147ca34dc1fbe554cb9c1baf2b97d6e4d26
Anasemzr <anasemzr@gmail.com>
2023-02-08 16:59:49 +0100

    Updated the command to get all objects "sondage" GET/api/sondage/get-all

--- api/src/main/java/com/apisae/api/controllers/sondage/SondageController.java
+++ api/src/main/java/com/apisae/api/controllers/sondage/SondageController.java
@@ -1,15 +1,15 @@
 package com.apisae.api.controllers.sondage;
 
-import com.apisae.api.models.sondage.Sondage;
+import com.apisae.api.projections.SondageProjection;
 import com.apisae.api.services.sondage.IServiceSondage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 
 @RequestMapping(path = "/api/sondage")
 @RestController
@@ -20,7 +20,7 @@ public class SondageController {
     private final IServiceSondage serviceSondage;
 
     @GetMapping(path ="/get-all")
-    public List<Sondage> getAllSondage(){
-        return serviceSondage.findAll();
+    public List<SondageProjection> getAllSondage(){
+        return serviceSondage.findAllSondage();
     }
 }
